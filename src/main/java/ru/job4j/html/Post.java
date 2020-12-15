c9675e6ec2a66eb3baadde14d1600aa9def8040f
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-12-15 14:25:50 +0300

    2.2. Парсинг даты.[285211#287570]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,9 @@
+package ru.job4j.html;
+
+public class Post {
+    private String postName;
+    private String link;
+    private String author;
+    private String text;
+    private String date;
+}
