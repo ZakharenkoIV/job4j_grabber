2659e8f08954dcecf95ff5480534c1ef89009644
ZakharenkoIV <zaharenkoiv93@gmail.com>
2020-12-16 14:20:12 +0300

    2.1. Парсинг https://www.sql.ru/forum/job-offers/3[285210#287569]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class SqlRuParse {
     public Post fillThePost(Post post, String linkToPost) throws IOException {
@@ -19,12 +20,17 @@ public class SqlRuParse {
         return new Post();
     }
 
-    public void parse(int firstPage, int lastPage) throws IOException {
+    public void parse(int firstPage, int lastPage) {
         int page = firstPage;
-        String reference = "https://www.sql.ru/forum/job-offers/" + page;
         while (page <= lastPage) {
-            Document doc = Jsoup.connect(reference).get();
-            Elements tdElements = doc.select("#content-wrapper-forum > table.forumTable > tbody > tr:gt(0)");
+            String url = "https://www.sql.ru/forum/job-offers/" + page++;
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements tdElements = Objects.requireNonNull(doc).select("#content-wrapper-forum > table.forumTable > tbody > tr:gt(0)");
             for (Element td : tdElements) {
                 if (!td.child(1).text().startsWith("Важно:")) {
                     System.out.println(td.child(1).child(0).attr("href"));
@@ -32,25 +38,10 @@ public class SqlRuParse {
                     System.out.println(td.child(5).text() + System.lineSeparator());
                 }
             }
-            reference = getNewReference(doc, ++page);
-            if (reference == null) {
-                break;
-            }
         }
-    }
-
-    private String getNewReference(Document doc, int page) {
-        String newReference = null;
-        Elements aElements = doc.select("#content-wrapper-forum > table:nth-child(7) > tbody > tr > td:nth-child(1) > a:gt(0)");
-        for (Element a : aElements) {
-            if (Integer.parseInt(a.text()) == page) {
-                newReference = a.attr("href");
-            }
-            break;
-        }
-        return newReference;
     }
 
     public static void main(String[] args) {
+        new SqlRuParse().parse(1, 5);
     }
 }
